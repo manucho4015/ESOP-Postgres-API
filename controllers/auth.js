0b716d7e1efc53e2fd360c362a8927047ac5e1d3
Emmanuel Ochieng <eochieng16@gmail.com>
2022-09-15 15:05:39 +0300

    Add login controller

---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+const CustomAPIError = require("../errors/custom-error");
+
+const db = require("../connect/db");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const query = await db.query(`SELECT * FROM "Users" where "Email"=($1) `, [
+    email,
+  ]);
+  if (query.rows.length === 0) {
+    throw new CustomAPIError(StatusCodes.NOT_FOUND, "User not found!");
+  }
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    query.rows[0].Password
+  );
+  if (!isPasswordCorrect) {
+    throw new CustomAPIError(StatusCodes.BAD_REQUEST, "Invalid Password");
+  }
+
+  const token = jwt.sign({ email: email }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  res.status(StatusCodes.OK).json({ msg: "Logged In", token });
+};
+
+module.exports = login;
