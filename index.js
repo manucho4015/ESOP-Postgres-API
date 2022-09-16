72685a7316581ac7795fff3418c4358a7c912211
Emmanuel Ochieng <eochieng16@gmail.com>
2022-09-16 14:03:39 +0300

    swagger docs added

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -26,8 +31,9 @@ const authenticationMiddleware = require("./middleware/authenticator");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.get("/", (req, res) => {
-  res.send("ESOP Postgres API");
+  res.send("<h1>ESOP Postgres API</h1><a href='api-docs'>Document</a>");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 const authRouter = require("./routes/auth");
